c07354f0069b11816106f14b24f0ec13c4a15b9f
Supriya <supriya.nandi@nagarro.com>
2019-09-28 17:01:16 +0530

    api  testing

--- src/test/java/Autothon/test/APITesting.java
+++ src/test/java/Autothon/test/APITesting.java
@@ -45,5 +45,65 @@ public class APITesting extends Base {
         } catch (Exception e) {
 
         }
+        
+        static String token ="";
+        public static void getToken(String[] args) {
+               
+               RestTemplate restTemplate;
+               HttpHeaders headers;
+               MultiValueMap<String, String> map = null;
+               HttpEntity<MultiValueMap<String, String>> requestEntity;
+               ResponseEntity<Quote> quote;
+               String jsonBody = "{\"username\" : \"admin\",\"password\" : \"password\"}";
+               try {
+                      restTemplate = new RestTemplate();
+                      headers = new HttpHeaders();
+                      headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                      HttpEntity<MultiValueMap<String, String>>
+                      requestEntity1 = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+                      quote = restTemplate.exchange(new URI("http://autothon-nagarro-backend-b04.azurewebsites.net/login"),
+                                   HttpMethod.POST, requestEntity1, Quote.class);
+                      token = quote.getBody().getToken();
+               } catch (Exception e) {
+
+               }
+        }
+        
+        
+        public static void addMovie(String[] args) {
+
+               RestTemplate restTemplate;
+               HttpHeaders headers;
+               MultiValueMap<String, String> map = null;
+               HttpEntity<MultiValueMap<String, String>> requestEntity;
+               ResponseEntity<Quote> quote;
+               String jsonBody = "{\"movie\":\n" + 
+                            "{\n" + 
+                            "      \"rating\":5,\n" + 
+                            "      \"title\":\"Test Test Test\",\n" + 
+                            "      \"description\":\"Nice movie nice\",\n" + 
+                            "      \"categories\":[\"Comedy\"],\n" + 
+                            "      \"image\":\"http://autothon-nagarro-frontend-b04.azurewebsites.net/addMovie\",\n" + 
+                            "      \"director\":\"Test Test Test\",\n" + 
+                            "      \"reviews\":[],\n" + 
+                            "      \"rented\":false\n" + 
+                            "}\n" + 
+                            "}";
+               try {
+                      map.add("Content-Type", "application/json");
+                      map.add("user", token);
+                      restTemplate = new RestTemplate();
+                      headers = new HttpHeaders();
+                      headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                      HttpEntity<MultiValueMap<String, String>>
+                      requestEntity1 = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+                      quote = restTemplate.exchange(new URI("http://autothon-nagarro-backend-b04.azurewebsites.net/login"),
+                                   HttpMethod.POST, requestEntity1, Quote.class);
+                      token = quote.getBody().getToken();
+               } catch (Exception e) {
+
+               }
+        }
+
  }
 }
